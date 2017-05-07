fa91fa860f551454121739d98f60fa49d28a40bf
Guilherme6191 <gui.vasconcelos.21@gmail.com>
2017-05-07 20:21:01 -0300

    Added redux.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import appReducers from './reducers'
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
+let store = createStore(appReducers)
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
